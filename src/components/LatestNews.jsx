7a7b5fb55b0df073de5976b9e65d76786045e18e
kawsarhossen1 <kawsarhossen66@gmail.com>
2025-04-21 10:08:13 +0600

    nav section added

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 const LatestNews = () => {
     return (
-        <div className='text-start flex gap-2 items-center bg-base-200'>
+        <div className='text-start flex gap-2 items-center bg-base-200 p-2'>
             <p className='bg-[#D72050] font-bold rounded-md text-white px-3 py-1'>Latest</p>
-            <Marquee className='space-x-10'>
+            <Marquee pauseOnHover={true} className='space-x-10'>
                 <Link to="/news">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut natus, iste officia adipisci cumque deserunt nesciunt quod quis, maxime unde, necessitatibus non quasi repellendus earum quo esse voluptatibus pariatur expedita?</Link>
                 <Link to="/news">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut natus, iste officia adipisci cumque deserunt nesciunt quod quis, maxime unde, necessitatibus non quasi repellendus earum quo esse voluptatibus pariatur expedita?</Link>
                 <Link to="/news">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut natus, iste officia adipisci cumque deserunt nesciunt quod quis, maxime unde, necessitatibus non quasi repellendus earum quo esse voluptatibus pariatur expedita?</Link>
---
+++ src/components/RightNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightNav = () => {
+    return (
+        <div>
+            Right Nav is Here
+        </div>
+    );
+};
+
+export default RightNav;
