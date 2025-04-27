5a6b30c91747cbb882cd45117cfcdd4e4fe16d8f
kawsarhossen1 <kawsarhossen66@gmail.com>
2025-04-27 10:02:42 +0600

    project started for aauthentication

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const AuthLayout = () => {
+  return (
+    <div>
+      Auth Layout <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default AuthLayout;
